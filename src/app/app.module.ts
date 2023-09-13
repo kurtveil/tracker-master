861e968437f58a2da6e83bb5a7f9c43684422b6d
kurt <kurtveil1@gmail.com>
2023-09-12 21:40:57 -0500

    [ADD] created components

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { SearchComponent } from './search/search.component';
+import { InformationComponent } from './information/information.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MapComponent,
+    SearchComponent,
+    InformationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
