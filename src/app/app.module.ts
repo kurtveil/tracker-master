effd3b870e1eb341c90f64082007ca3048679a57
kurt <kurtveil1@gmail.com>
2023-09-14 00:21:47 -0500

    [ADD] fix css and html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
-import { SearchComponent } from './components/search/search.component';
-import { InformationComponent } from './components/information/information.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    SearchComponent,
-    InformationComponent
   ],
   imports: [
     BrowserModule,
--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { environment } from '../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class MapService {
   private baseUrl: string  = environment.base_url;
   constructor(private http: HttpClient) { }
